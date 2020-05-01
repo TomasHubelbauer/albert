24e2e7c7e2cd6496ceb0ee31c10b3d3e4d924c0a
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-01 14:59:39 +0200

    Demonstrate extracting images and texts with bounds

--- index.js
+++ index.js
@@ -3,112 +3,113 @@ const path = require('path');
 const fs = require('fs-extra');
 
 void async function () {
+  await fs.remove('data');
+  await fs.ensureDir('data');
+
   const browser = await playwright.firefox.launch({ headless: false });
   try {
     const page = await browser.newPage();
     await page.goto('file://' + path.join(__dirname, 'hypermarket-49-2019.pdf'));
+    await page.waitForFunction(() => PDFViewerApplication && PDFViewerApplication.pdfDocument);
 
-    // Wait for the document and the viewer to load
-    await page.waitForFunction(() => PDFViewerApplication.pdfDocument);
+    const length = await page.evaluate(() => PDFViewerApplication.pdfDocument.numPages);
+    for (let index = 0; index < length; index++) {
+      const number = index + 1;
+      console.log(`Scraping ${number}/${length}`);
 
-    // Use PDF.js API to extract image bounds (flattened on the `canvas` unlike texts which are their own `spans`)
-    // TODO: Extract texts from the spans instead because it is more straightforward then unrolling the ops
-    // TODO: Extract images some other way because it is too finicky from the ops and args (canvas metadata, PDFViewerApplication fields?)
-    const data = await page.evaluate(async () => {
-      const doc = PDFViewerApplication.pdfDocument;
-      const pages = {};
-      for (let number = 1; number <= 0 /* document.numPages */; number++) {
-        const page = await doc.getPage(number);
-        pages[number] = { texts: [], images: [] };
+      const item = await page.evaluate(async (number) => {
+        const item = { images: [], texts: [] };
+        const page = await PDFViewerApplication.pdfDocument.getPage(number);
+        const viewport = page.getViewport({ scale: 1 });
 
-        let textMatrix;
-        let imageMatrix;
-        const ops = await page.getOperatorList();
-        for (let index = 0; index < ops.fnArray.length; index++) {
-          const fn = ops.fnArray[index];
-          const args = ops.argsArray[index];
+        // Create a new canvas for each page otherwise PDF.js throws
+        const canvas = document.createElement('canvas');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
 
-          switch (fn) {
-            case pdfjsLib.OPS.setTextMatrix: {
-              textMatrix = args;
-              break;
+        document.body.innerHTML = '';
+        document.body.append(canvas);
+
+        const context = canvas.getContext('2d');
+        let undefer;
+        const deferred = new Promise(resolve => undefer = resolve);
+        const imageLayer = {
+          beginLayout: () => { },
+          endLayout: undefer,
+          appendImage: ({ left: x, top: y, width, height, imgData }) => {
+            console.log(number, x, y, width, height, imgData || 'no data!');
+            if (!imgData) {
+              item.images.push({ x, y, width, height });
+              return;
             }
-            case pdfjsLib.OPS.showText: {
-              if (args.length !== 1) {
-                throw new Error('Expected text to be an array with a single array element.');
-              }
 
-              const text = args[0].filter(a => a.unicode).map(a => a.unicode).join('').trim();
-              if (!text) {
+            const canvas = document.createElement('canvas');
+            canvas.width = imgData.width;
+            canvas.height = imgData.height;
+            const context = canvas.getContext('2d');
+
+            /** @type {Uint8ClampedArray} */
+            let array;
+            switch (imgData.data.length) {
+              case imgData.width * imgData.height * 3: {
+                array = new Uint8ClampedArray(imgData.width * imgData.height * 4);
+                for (let index = 0; index < array.length; index++) {
+                  // Set alpha channel to full
+                  if (index % 4 === 3) {
+                    array[index] = 17;
+                  }
+                  // Copy RGB channel components from the original array
+                  else {
+                    array[index] = imgData.data[~~(index / 4) * 3 + (index % 4)];
+                  }
+                }
+
                 break;
               }
-
-              // For the matrix see https://github.com/mozilla/pdf.js/issues/10498
-              pages[number].texts.push({ text, x: textMatrix[4], y: page.view[3] - textMatrix[5] + textMatrix[3] })
-              break;
+              case imgData.width * imgData.height * 4: {
+                array = imgData.data;
+                break;
+              }
+              default: {
+                alert('Unknown imgData format!');
+              }
             }
-            case pdfjsLib.OPS.transform: {
-              imageMatrix = args;
-              break;
-            }
-            case pdfjsLib.OPS.paintImageXObject: {
-              const obj = await page.objs.get(args[0]);
 
-              /** @type {Uint8ClampedArray} */
-              const _data = obj.data;
+            context.putImageData(new ImageData(array, imgData.width, imgData.height), 0, 0);
+            const data = { width: imgData.width, height: imgData.height, url: canvas.toDataURL() };
+            item.images.push({ x, y, width, height, data });
+          },
+        };
 
-              /** @type {Number} */
-              const width = obj.width;
+        page.render({ canvasContext: context, viewport, imageLayer });
+        await deferred;
 
-              /** @type {Number} */
-              const height = obj.height;
-
-              // TODO: Replace img with canvas and push these as ImageData to it
-              // Swap lines to go top to bottom instead of bottom to top
-              // const data = new Uint8Array(_data.length);
-              // for (let y = 0; y < height; y++) {
-              //   data.set(_data.slice((height - y - 1) * width * 3, (height - y - 1) * width * 3 + width * 3), y * width * 3);
-
-              //   // Convert from BGR to RGB
-              //   for (let x = 0; x < width; x++) {
-              //     const offset = y * width * 3 + x * 3;
-              //     const slice = data.slice(offset, offset + 3).reverse();
-              //     data.set(slice, offset);
-              //   }
-              // }
-
-              // For the matrix see https://github.com/mozilla/pdf.js/issues/10498
-              pages[number].images.push({ x: imageMatrix[4], y: page.view[3] - imageMatrix[5] + imageMatrix[3] });
-
-              // TODO: Figure out why the scale here gives weird numbers, do the previous transforms affect this?
-              // If yes I might need to keep the track of them and multiply them as they come to get the correct scale here.
-              // const [_scaleX, _skewY, _skewX, _scaleY, transformX, transformY] = imageMatrix;
-
-              // TODO: Adjust `y` to be distance from top not from bottom (PDF default) by subtracting it from `page.view`
-              // const x = Math.round(transformX);
-              // const y = Math.round(transformY);
-              break;
-            }
-          }
+        for (const { transform: [, , , , x, y], width, height, str: data } of (await page.getTextContent()).items) {
+          item.texts.push({ x, y: viewport.height - y - height, width, height, data });
         }
+
+        return item;
+      }, number);
+
+      await fs.ensureDir('data/' + number);
+
+      let counter = 0;
+      for (const image of item.images) {
+        counter++;
+        if (!image.data) {
+          continue;
+        }
+
+        const buffer = new Buffer.from(image.data.url.slice('data:image/png;base64,'.length), 'base64');
+        await fs.writeFile(`data/${number}/${counter}.png`, buffer);
+
+        delete image.data.url;
+        image.data.name = counter + '.png';
       }
 
-      const data = { pages };
-      const keys = Object.keys(PDFViewerApplication);
-      for (const key of keys) {
-        const type = typeof PDFViewerApplication[key];
-        if (type === 'object') {
-          data[key] = !!PDFViewerApplication[key] ? Object.keys(PDFViewerApplication[key]) : null;
-        }
-        else {
-          data[keys] = type;
-        }
-      }
-
-      return data;
-    });
-
-    await fs.writeJSON('data.json', data, { spaces: 2 });
+      await fs.writeJSON(`data/${number}/page.json`, item, { spaces: 2 });
+      console.log(`Content saved ${number}/${length}`);
+    }
   }
 
   // Close the browser on error
--- readme.md
+++ readme.md
@@ -10,12 +10,4 @@ extract the item price and photo associations.
 
 ## To-Do
 
-### Scrap using ops and args and see if I can extract image bounds from PDF.js
-
-I would have to reimplement PDF.js if I went by the ops and args, instead, it is
-needed to inspect the `PDFViewerApplication` for fields (including nested) which
-could be used to extract image bounds or to monkey-patch the PDF.js rendered so
-that when it renders an image onto the canvas, it also passes along the bounds
-to me.
-
-Use https://mozilla.github.io/pdf.js/web/viewer.html to debug PDF.js.
+### Cluster product images and the corresponding price and discount texts
