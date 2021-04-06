bdcc299666561cbdc631d3a1b319d9a5affde9a5
Tomas Hubelbauer <tomas@hubelbauer.net>
2021-04-06 22:08:11 +0200

    Extract the PDF URL to its own module

--- extract.js
+++ extract.js
@@ -1,5 +1,6 @@
 import playwright from 'playwright-firefox';
 import fs from 'fs';
+import url from './url.js';
 
 void async function () {
   try {
@@ -15,8 +16,6 @@ void async function () {
 
   const browser = await playwright.firefox.launch({ headless: false });
   const page = await browser.newPage();
-  const now = new Date();
-  const url = `https://www.albert.cz/aktualni-letaky/supermarket-${now.getWeek()}-${now.getWeekYear()}-city.pdf?field=pdf`;
   await page.goto(url);
   await page.waitForFunction(() => PDFViewerApplication && PDFViewerApplication.pdfDocument);
 
@@ -140,22 +139,3 @@ void async function () {
 
   await browser.close();
 }()
-
-// https://weeknumber.net/how-to/javascript
-Date.prototype.getWeek = function () {
-  var date = new Date(this.getTime());
-  date.setHours(0, 0, 0, 0);
-  // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  // January 4 is always in week 1.
-  var week1 = new Date(date.getFullYear(), 0, 4);
-  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
-}
-
-// https://weeknumber.net/how-to/javascript
-Date.prototype.getWeekYear = function () {
-  var date = new Date(this.getTime());
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  return date.getFullYear();
-}
